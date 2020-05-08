3d948f3a9845b47f49a15eac18abeea5f8f58fca
Aki-7 <aki.develop8128@gmail.com>
2020-05-08 16:53:19 +0000

    ssl with own ca

---
+++ client.py
@@ -0,0 +1,30 @@
+import ssl
+import socket
+import os
+from pathlib import Path
+
+hostname="localhost"
+
+def passwd():
+    with open("passwd", "r") as f:
+        pswd = f.readline().rstrip(os.linesep)
+    return pswd
+
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.load_verify_locations(cafile=Path("certs/ca.crt.pem"))
+
+context.load_cert_chain(
+    certfile='certs/clt.chain.pem',
+    keyfile='certs/clt.pem',
+    password=passwd,
+)
+
+conn = context.wrap_socket(
+    socket.socket(socket.AF_INET),
+    server_hostname=hostname
+)
+
+conn.connect((hostname, 5000))
+
+conn.send(b"hello")
---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+import ssl
+import os
+from pathlib import Path
+
+
+def passwd():
+    with open("passwd", "r") as f:
+        pswd = f.readline().rstrip(os.linesep)
+    return pswd
+
+
+context = ssl.create_default_context(
+    ssl.Purpose.CLIENT_AUTH,
+    cafile=Path("certs/ca.crt.pem")
+)
+context.verify_mode = ssl.CERT_REQUIRED
+
+context.load_cert_chain(
+    certfile='certs/svr.chain.pem',
+    keyfile='certs/svr.pem',
+    password=passwd,
+)
+
+socket = socket.socket()
+socket.bind(("localhost", 5000))
+socket.listen(5)
+
+
+def deal_with_client(connstream):
+    data = connstream.read()
+    while data:
+        print(data)
+        data = connstream.read()
+
+
+while True:
+    newsock, addr = socket.accept()
+    connstream = context.wrap_socket(newsock, server_side=True)
+    try:
+        deal_with_client(connstream)
+    finally:
+        connstream.close()
